35ce98bc904b3586b4f867cc3d6c86af41eea1d7
kogoto14 <oneokrock.tennis@gmail.com>
2025-09-21 11:51:16 +0900

    Validator 追加

--- src/main/java/jp/co/monocrea/user/resource/UserResource.java
+++ src/main/java/jp/co/monocrea/user/resource/UserResource.java
@@ -1,6 +1,10 @@
 package jp.co.monocrea.user.resource;
 
 import jakarta.inject.Inject;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -17,12 +21,12 @@ public class UserResource {
 
     @GET
     public Response getPagenatedUsers(
-        @QueryParam("id") Integer id,
+        @QueryParam("id") @Positive Integer id,
         @QueryParam("_name_like") String nameLike,
-        @QueryParam("_sort") String sortKey,
+        @QueryParam("_sort") @Pattern(regexp = "id|name|createdAt|updatedAt") String sortKey,
         @QueryParam("_order") @DefaultValue("ASC") OrderEnum order,
-        @QueryParam("_page") Integer page,
-        @QueryParam("_limit") Integer limit
+        @QueryParam("_page")  @DefaultValue("1") Integer page,
+        @QueryParam("_limit") @DefaultValue("10") @Min(1) @Max(100) Integer limit
     ) {        
         UserSummariesDto userSummaries = userService.getPagenatedUserSummaries(id, nameLike, sortKey, order, page, limit);
         return Response
